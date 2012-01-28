f0967118a208804bc1eb341b70bb28682cf5ba79
Mike Heitzke <mike@isotope11.com>
2012-01-28 06:51:36 -0600

    moving the responsive layout to the application layout and making it the root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  layout 'responsive'
+  layout 'application'
 
   #######################################################################
   private
