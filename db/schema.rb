f82ef5e8ed4fb6201189736cc4af9452752d0ef8
Mike Heitzke <mike@isotope11.com>
2012-02-23 16:41:24 -0600

    styling for the posts with tag drop down thing, new post, edit post, login

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(:version => 20120223195139) do
   create_table "users", :force => true do |t|
     t.string   "email",                              :default => "", :null => false
     t.string   "encrypted_password",  :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                      :default => "", :null => false
     t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                      :default => 0
